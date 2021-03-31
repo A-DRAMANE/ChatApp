9936258f9bc406eb15a50158a77b0f63e9584729
A-DRAMANE <abdoulcommedramane@gmail.com>
2021-03-31 11:49:17 +0000

    send message to screen and style

--- src/components/TheirMessage.js
+++ src/components/TheirMessage.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
-const TheirMessage = (props) => {
+const TheirMessage = (lastMessage,message) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
     return (
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {<div
+                className="message-avatar"
+                style={{backgroundImage:'url(${message?.sender?.avatar})'}}
+            />}
+
+            {message?.attachment?.length>0
+                ?(
+                    <img
+                        src={message.attachment[0].fille}
+                        alt="message attachment"
+                        className="message-image"
+                        style={{marginLeft:isFirstMessageByUser ? "4px" : "48px"}}
+                    />
+                ) : (
+                    <div className="message" style={{float:"left", color:"white",backgroundColor:"#CABCDC",marginLeft:isFirstMessageByUser ? "4px" : "48px"}}>
+                        {message.texte}
+                    </div>
+                )
+        }
         </div>
     )
 }
